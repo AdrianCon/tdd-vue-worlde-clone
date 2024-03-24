f5a0971603ba1646298a813ecfa098642e1345d4
AdrianCon <adrian.dev@tutanota.com>
2024-03-24 13:45:48 -0600

    Extract arrange phase to beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -4,10 +4,12 @@ import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '../../settings'
 
 describe('WordleBoard', () => {
   let wordOfTheDay = 'TESTS'
+  let wrapper: ReturnType<typeof mount>;
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+  })
+
   test('A victory message appears when the user makes a guess that matches the word of the day', async () => {
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-    
     // Act phase
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue('TESTS')
@@ -18,9 +20,6 @@ describe('WordleBoard', () => {
   })
 
   test("A defeat message appears if the user makes a guess that is incorrrect", async () => {
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-    
     // Act phase
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue('WRONG')
@@ -31,9 +30,6 @@ describe('WordleBoard', () => {
   })
 
   test("No end-of-game appears if the user has not yet made a guess", async () => {
-    // Arrange phase
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
-    
     // Assert phase
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
