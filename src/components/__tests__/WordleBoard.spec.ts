839c2260e0b527e4f108eab7cf2257bc18339a0f
AdrianCon <adrian.dev@tutanota.com>
2024-03-24 21:23:50 -0600

    Separate helper methods for typing and submitting guesses

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -10,10 +10,17 @@ describe('WordleBoard', () => {
     wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
   })
 
+  async function playerTypesGuess(guess: string) {
+    await wrapper.find('input[type="text"]').setValue(guess)
+  }
+  
+  async function playerPressingEnter() { 
+    await wrapper.find('input[type="text"').trigger('keydown.enter')
+  }
+
   async function playerTypesAndSubmitsGuess(guess: string) {
-    const guessInput = wrapper.find('input[type="text"]')
-    await guessInput.setValue(guess)
-    await guessInput.trigger('keydown.enter')
+    await playerTypesGuess(guess)
+    await playerPressingEnter()
   }
 
   describe("End of game messages", () => { 
