d0d005b17af9cf68d21f56ba69e2f522df06f91d
AdrianCon <adrian.dev@tutanota.com>
2024-03-24 21:07:32 -0600

    Add shake animation when guess submission is rejected

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -9,6 +9,7 @@ withDefaults(defineProps<{disabled?: boolean}>(), {
 })
 
 const guessInProgress = ref<string | null>(null)
+const hasFailedValidation = ref<boolean>(false)
 const emit = defineEmits<{
   "guess-submitted": [guess: string]
 }>();
@@ -29,6 +30,10 @@ const formattedGuessInProgress = computed<string>({
 
 function onSubmit() {
   if (!englishWords.includes(formattedGuessInProgress.value)) {
+    hasFailedValidation.value = true
+    setTimeout(() => {
+      hasFailedValidation.value = false
+    }, 500)
     return
   }
 
@@ -38,7 +43,7 @@ function onSubmit() {
 </script>
 
 <template>
-  <GuessView v-if="!disabled" :guess="formattedGuessInProgress" />
+  <GuessView v-if="!disabled" :class="{shake: hasFailedValidation}" :guess="formattedGuessInProgress" />
   <input
     v-model="formattedGuessInProgress"
     :maxlength="WORD_SIZE"
@@ -56,36 +61,27 @@ input {
   opacity: 0;
 }
 
-.word {
-  list-style: none;
-  padding: 0;
-  display: flex;
-  gap: 0.25rem;
+.shake {
+  animation: shake;
+  animation-duration: 100ms;
+  animation-iteration-count: 2;
 }
 
-.letter {
-  background-color: white;
-  border: 1px solid hsl(0, 0%, 70%);
-  width: 5rem;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  font-weight: bolder;
-}
-
-li:not([data-letter=" "]) {
-  animation: pop 100ms;
-}
-
-@keyframes pop {
+@keyframes shake {
   0% {
-    transform: scale(1);
+    transform: translateX(-2%);
+  }
+
+  25% {
+    transform: translateX(0);
   }
 
   50% {
-    transform: scale(1.4);
+    transform: translateX(2%);
+  }
+
+  75% {
+    transform: translateX(0);
   }
 }
 </style>
