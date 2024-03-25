8997e59f99b7a101538ba46a5a4d6b5ce97eaee5
AdrianCon <adrian.dev@tutanota.com>
2024-03-24 19:45:48 -0600

    Extract guess input into dedicated component

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE, WORD_SIZE } from "@/settings"
 import englishWords from "@/englishWordsWith5Letters.json"
-import { computed, ref } from "vue"
+import { ref } from "vue"
+import GuessInput from "@/components/GuessInput.vue"
 
 defineProps({
   wordOfTheDay: {
@@ -9,33 +10,12 @@ defineProps({
     validator: (wordGiven: string) => englishWords.includes(wordGiven)
   }
 })
-const guessInProgress = ref<string | null>(null)
+
 const guessSubmitted = ref("")
 
-const formattedGuessInProgress = computed<string>({
-  get() {
-    return guessInProgress.value ?? ""
-  },
-  set(rawValue: string) {
-    guessInProgress.value = null
-
-    guessInProgress.value = rawValue
-      .slice(0, WORD_SIZE)
-      .toUpperCase()
-      .replace(/[^A-Z]+/gi, "")
-  }
-})
-
-function onSubmit() {
-  if (!englishWords.includes(formattedGuessInProgress.value)) {
-    return
-  }
-
-  guessSubmitted.value = formattedGuessInProgress.value
-}
 </script>
 
 <template>
-  <input v-model="formattedGuessInProgress" :maxlength="WORD_SIZE" type="text" @keydown.enter="onSubmit">
+  <GuessInput @guess-submitted="(guess: string) => guessSubmitted = guess" />
   <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
 </template>
